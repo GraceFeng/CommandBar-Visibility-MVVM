36e871f82fdc8ca3fa33fad07b1eb1319985a504
Grace-Feng <v-linfen@microsoft.com>
2016-03-22 16:09:27 +0800

    Commandbar visibility MVVM

---
+++ ChangeVisibilityOfButtonFromCodeMVVN/Converter/NaviButtonShowOrNot.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace ChangeVisibilityOfButtonFromCodeMVVN.Converter
+{
+    public class NaviButtonShowOrNot : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            Uri uri = new Uri(value.ToString());
+            if (uri != null)
+            {
+                if (uri.Equals("ms-appx:///MainPage.xaml"))
+                {
+                    return Visibility.Collapsed;
+                }
+            }
+            return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ChangeVisibilityOfButtonFromCodeMVVN/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+namespace ChangeVisibilityOfButtonFromCodeMVVN.ViewModel
+{
+    public class MainPageViewModel
+    {
+        public MainPageViewModel()
+        {
+        }
+
+        public void hub_SectionHeaderClick(object sender, HubSectionHeaderClickEventArgs e)
+        {
+            Hub hub = sender as Hub;
+            Frame frame = (Frame)hub.Parent;
+            switch (e.Section.Name)
+            {
+                case "image1":
+                    frame.Navigate(typeof(View.Page1));
+                    break;
+
+                case "image2":
+                    frame.Navigate(typeof(View.Page2));
+                    break;
+
+                case "image3":
+                    frame.Navigate(typeof(View.Page3));
+                    break;
+            }
+        }
+
+        public void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Frame rootFrame = Window.Current.Content as Frame;
+            if (rootFrame != null)
+            {
+                rootFrame.Navigate(typeof(MainPage));
+            }
+        }
+    }
+}
